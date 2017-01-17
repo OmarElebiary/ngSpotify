677cb792543d428edb43dd736b3229158c399008
OmarElebiary <es-omar.khairy1217@alexu.edu.eg>
2017-01-17 14:30:05 +0200

    Initial Commit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,21 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+
+import { routing } from './app.router';
+import { SpotifyService } from './services/spotify.service';
 
 import { AppComponent }  from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistComponent } from './components/artist/artist.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, FormsModule, HttpModule, JsonpModule, routing ],
+  declarations: [ AppComponent, NavbarComponent, AboutComponent, SearchComponent, ArtistComponent ],
+  providers: [ SpotifyService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { routing } from './app.router';
 
 import { AppModule } from './app.module';
 
